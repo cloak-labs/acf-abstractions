2fcf7104055c9ac061836d3f0a56f3bc82ff6c96
Kaelan Smith <kaelancsmith@gmail.com>
2025-03-17 18:56:59 -0700

    refactor MenuSelect & PostTypeSelect to extend PrepopulatedSelect

--- src/Fields/MenuSelect.php
+++ src/Fields/MenuSelect.php
@@ -2,61 +2,25 @@
 
 namespace CloakWP\ACF\Fields;
 
-use Extended\ACF\Fields\Select;
-use InvalidArgumentException;
+use CloakWP\ACF\Traits\ConfigurableChoices;
 
 /**
- * An auto-populated ACF select field that allows you to select a WordPress menu.
+ * An ACF select field auto-populated with WordPress menus.
  */
-class MenuSelect extends Select
+class MenuSelect extends PrepopulatedSelect
 {
-  private function get_valid_menus()
-  {
-    $menus = array();
-    $raw_menus = wp_get_nav_menus();
+  use ConfigurableChoices;
 
-    foreach ($raw_menus as $menu) {
+  // No hook needed for menus as they're available immediately
+  protected ?string $setHook = null;
+
+  protected function setChoices(): void
+  {
+    $menus = [];
+    foreach (wp_get_nav_menus() as $menu) {
       $menus[$menu->term_id] = $menu->name;
     }
 
-    return $menus;
-  }
-
-  // we override inherited `make` in order to set default post type options when include() isn't called/specified
-  public static function make(string $label, string|null $name = null): static
-  {
-    $self = new static($label, $name);
-    $self->include(); // set defaults
-    return $self;
-  }
-
-  public function include(array $enabledMenuSlugs = null): self
-  {
-    add_action("init", function () use ($enabledMenuSlugs) {
-      $validChoices = $this->get_valid_menus();
-      $choices = [];
-
-      if ($enabledMenuSlugs) {
-        if (is_array($enabledMenuSlugs)) {
-          foreach ($enabledMenuSlugs as $choice) {
-            if (!array_key_exists($choice, $validChoices)) {
-              throw new InvalidArgumentException("Non-existant menu slug provided to include method: $choice");
-            }
-
-            // Set the choices field based on enabled choices
-            $choices[$choice] = $validChoices[$choice];
-          }
-        } else {
-          throw new InvalidArgumentException("Invalid inclusion value -- must be ARRAY of menu slug strings.");
-        }
-      } else {
-        $choices = $validChoices;
-      }
-
-      // Add choices to settings
-      $this->choices($choices);
-    }, 4);
-
-    return $this;
+    $this->choices($menus);
   }
 }
--- src/Fields/PostTypeSelect.php
+++ src/Fields/PostTypeSelect.php
@@ -4,79 +4,32 @@ namespace CloakWP\ACF\Fields;
 
 use CloakWP\Core\PostReturnType;
 use CloakWP\Core\Utils;
-use Extended\ACF\Fields\Select;
-use InvalidArgumentException;
+use CloakWP\ACF\Traits\ConfigurableChoices;
 
 /**
  * An auto-populated ACF select field that allows you to select a WordPress post type.
  */
-class PostTypeSelect extends Select
+class PostTypeSelect extends PrepopulatedSelect
 {
-  /** Note: get_valid_post_types should be called during/after the "init" hook in order for post types to be ready.  */
-  private function get_valid_post_types()
+  use ConfigurableChoices;
+
+  // Post types need to be loaded after init
+  protected ?string $setHook = 'after_setup_theme';
+
+  protected function setChoices(): void
   {
-    $post_types = array();
-    $exclude = array('attachment');
-    $customPostTypes = Utils::get_custom_post_types(PostReturnType::Objects);
-    $publicPostTypes = Utils::get_public_post_types(PostReturnType::Objects);
-    $allPostTypes = array_merge($customPostTypes, $publicPostTypes);
+    $customPostTypes = Utils::getCustomPostTypes(PostReturnType::Objects);
+    $publicPostTypes = Utils::getPublicPostTypes(PostReturnType::Objects);
 
-    foreach ($allPostTypes as $post_slug => $post_type_object) {
-      // Bail early if post type is excluded.
-      if (in_array($post_slug, $exclude)) {
+    $exclude = ['attachment'];
+    $postTypes = [];
+    foreach (array_merge($customPostTypes, $publicPostTypes) as $slug => $post) {
+      // Bail early if post type is excluded
+      if (in_array($slug, $exclude))
         continue;
-      }
-
-      // Bail early if is builtin (WP) private post type
-      // i.e. nav_menu_item, revision, customize_changeset, etc.
-      // if ($post_type_object->_builtin && !$post_type_object->public) {
-      //   continue;
-      // }
-
-      $label = $post_type_object->labels->singular_name;
-      $post_types[$post_slug] = $label;
+      $postTypes[$slug] = $post->labels->singular_name;
     }
 
-    return $post_types;
-  }
-
-  // we override inherited `make` in order to set default post type options when include() isn't called/specified
-  public static function make(string $label, string|null $name = null): static
-  {
-    $self = new static($label, $name);
-    // $post_types = $self->get_valid_post_types();
-    $self->include(); // set defaults
-    return $self;
-  }
-
-  public function include(array $enabledPostTypes = null): self
-  {
-    add_action("init", function () use ($enabledPostTypes) {
-      $validChoices = $this->get_valid_post_types();
-
-      $choices = [];
-      if ($enabledPostTypes) {
-        if (is_array($enabledPostTypes)) {
-          foreach ($enabledPostTypes as $choice) {
-            if (!array_key_exists($choice, $validChoices)) {
-              continue;
-              // throw new InvalidArgumentException("Invalid post type choice: $choice");
-            }
-
-            // Set the choices field based on enabled choices
-            $choices[$choice] = $validChoices[$choice];
-          }
-        } else {
-          throw new InvalidArgumentException("Invalid inclusion value -- must be ARRAY of post label strings.");
-        }
-      } else {
-        $choices = $validChoices;
-      }
-
-      // Add choices to settings
-      $this->choices($choices);
-    }, 4);
-
-    return $this;
+    $this->choices($postTypes);
   }
 }
