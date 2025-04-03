84512dff453bb3e1960e32c8d6a5bef1a0b21d4f
Kaelan Smith <kaelancsmith@gmail.com>
2025-04-03 08:52:00 -0700

    update MenuSelect to reflect setHook deletion

--- src/Fields/MenuSelect.php
+++ src/Fields/MenuSelect.php
@@ -2,18 +2,11 @@
 
 namespace CloakWP\ACF\Fields;
 
-use CloakWP\ACF\Traits\ConfigurableChoices;
-
 /**
  * An ACF select field auto-populated with WordPress menus.
  */
 class MenuSelect extends PrepopulatedSelect
 {
-  use ConfigurableChoices;
-
-  // No hook needed for menus as they're available immediately
-  protected ?string $setHook = null;
-
   protected function setChoices(): void
   {
     $menus = [];
